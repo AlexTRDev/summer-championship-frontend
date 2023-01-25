59933b480fb2e6bb5d8f898539d4766f2d6167d4
Alex Terrones Rivera <allex_tr@hotmail.com>
2023-01-24 22:51:22 -0500

    Update Logos.tsx

--- src/components/Logos/Logos.tsx
+++ src/components/Logos/Logos.tsx
@@ -9,15 +9,15 @@ export interface LogosInterface {
 const Logos: React.FC<LogosInterface> = ({ teams }) => {
   return (
     <div className="flex flex-row justify-center items-center content-center w-full p-2">
-      <div className="flex flex-row flex-wrap justify-center items-center gap-x-8">
+      <div className="flex flex-row flex-wrap justify-center items-center gap-x-8 ">
         {teams
-          .map(team => <img key={crypto.randomUUID()} src={imgTeam} alt={team.name} width={'33'} />)
+          .map(team => <img key={crypto.randomUUID()} src={imgTeam} alt={team.name} width={32} />)
           .slice(0, teams.length / 2)}
       </div>
       <img src={logo} alt="logo del campeonato" width={'100px'} />
-      <div className="flex flex-row flex-wrap justify-center items-center gap-x-8">
+      <div className="flex flex-row flex-wrap justify-center items-center gap-x-8 ">
         {teams
-          .map(team => <img key={crypto.randomUUID()} src={imgTeam} alt={team.name} width={'33'} />)
+          .map(team => <img key={crypto.randomUUID()} src={imgTeam} alt={team.name} width={32} />)
           .slice(teams.length / 2, teams.length)}
       </div>
     </div>
