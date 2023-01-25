27aeac63e09cae9745ed80bd0c52f3766cbe9e40
Alex Terrones Rivera <allex_tr@hotmail.com>
2023-01-24 22:51:48 -0500

    Create StatsNav.tsx

---
+++ src/components/Navbars/StatsNav/StatsNav.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+export interface StatsNavInterface {}
+
+const StatsNav: React.FC<StatsNavInterface> = () => {
+  return (
+    <nav className="w-64 bg-rose-500 flex flex-col justify-center items-start  gap-4">
+      <NavLink to={'clasificacion'}>Clasificación</NavLink>
+      <NavLink to={'mvp'}>MVP</NavLink>
+      <NavLink to={'goleadores'}>Goleadores</NavLink>
+      <NavLink to={'asistencias'}>Asistencias</NavLink>
+    </nav>
+  )
+}
+
+export default StatsNav
