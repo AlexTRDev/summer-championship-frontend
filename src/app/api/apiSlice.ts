bdf9f3a6d4209a36999ed5fd57e0833ce4563553
Alex Terrones Rivera <allex_tr@hotmail.com>
2023-01-24 22:48:41 -0500

    add apiSlice of RTK Query

---
+++ src/app/api/apiSlice.ts
@@ -0,0 +1,9 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+const API_URL = import.meta.env.VITE_API_URL_DEV || import.meta.env.VITE_API_URL_PROD
+
+export const apiSlice = createApi({
+  baseQuery: fetchBaseQuery({ baseUrl: API_URL }),
+  endpoints: () => ({}),
+  tagTypes: ['Matches', 'Days'],
+})
