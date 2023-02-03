563dea17083457a15b914792a05d1c62712bd5ff
Alex Terrones Rivera <allex_tr@hotmail.com>
2023-02-02 20:47:35 -0500

    Update StatsNav.tsx

--- src/components/Navbars/StatsNav/StatsNav.tsx
+++ src/components/Navbars/StatsNav/StatsNav.tsx
@@ -4,12 +4,41 @@ export interface StatsNavInterface {}
 
 const StatsNav: React.FC<StatsNavInterface> = () => {
   return (
-    <nav className="w-64 bg-rose-500 flex flex-col justify-start items-start  gap-4">
-      <NavLink to={'clasificacion'}>Clasificación</NavLink>
-      <NavLink to={'mvp'}>MVP</NavLink>
-      <NavLink to={'goleadores'}>Goleadores</NavLink>
-      <NavLink to={'asistencias'}>Asistencias</NavLink>
-    </nav>
+    <>
+      <nav className="hidden bg-rose-500 sm:w-64  sm:flex flex-col justify-start items-start gap-4 ">
+        <NavLink to={'clasificacion'}>Clasificación</NavLink>
+        <NavLink to={'mvp'}>MVP</NavLink>
+        <NavLink to={'goleadores'}>Goleadores</NavLink>
+        <NavLink to={'asistencias'}>Asistencias</NavLink>
+      </nav>
+      {/* bg-gradient-to-bl from-blue-600 to-blue-400 */}
+      <nav className="grid grid-cols-2 sm:hidden gap-4 p-2">
+        <NavLink
+          to={'clasificacion'}
+          className={'p-2 bg-cornflower-blue-500 shadow-lg shadow-cornflower-blue-400/50 drop-shadow-lg'}
+        >
+          Clasificación
+        </NavLink>
+        <NavLink
+          to={'mvp'}
+          className={'p-2 bg-cornflower-blue-500 shadow-lg shadow-cornflower-blue-400/50 drop-shadow-lg'}
+        >
+          MVP
+        </NavLink>
+        <NavLink
+          to={'goleadores'}
+          className={'p-2 bg-cornflower-blue-500 shadow-lg shadow-cornflower-blue-400/50 drop-shadow-lg'}
+        >
+          Goleadores
+        </NavLink>
+        <NavLink
+          to={'asistencias'}
+          className={'p-2 bg-cornflower-blue-500 shadow-lg shadow-cornflower-blue-400/50 drop-shadow-lg'}
+        >
+          Asistencias
+        </NavLink>
+      </nav>
+    </>
   )
 }
 
