f004654101d1dde18c05f856a5f82d949079de57
Alex Terrones Rivera <allex_tr@hotmail.com>
2023-02-02 20:47:46 -0500

    Update CalendarLayout.tsx

--- src/layouts/CalendarLayout.tsx
+++ src/layouts/CalendarLayout.tsx
@@ -21,8 +21,8 @@ function CalendarLayout() {
     <>
       {/* <Match teams={teamAPI.data ?? []} /> */}
       <div>
-        <div className="bg-violet-500/50 p-4 w-full ">
-          <div className="bg-white flex flex-col">
+        <div className="bg-white p-4 w-full ">
+          <div className="bg-transparent flex flex-col">
             {/* select to journey */}
             <div className="text-2xl font-medium">
               <select name="match" id="match_1" onChange={handleSelectDay} value={selectDay}>
@@ -38,54 +38,59 @@ function CalendarLayout() {
                 ))}
               </select>
             </div>
-            <section className="p-2 bg-black/50">
+            <section className="p-2 bg-transparent">
               <div>
                 {dataCalendars?.calendars
                   ?.filter(t => t.awayTeamId !== null)
-                  .map(calendar => (
-                    <div
-                      key={crypto.randomUUID()}
-                      className="my-2 bg-rose-500 sm:py-2 grid grid-rows-3 items-center justify-center sm:grid-cols-3 sm:grid-rows-1 gap-4 hover:bg-gray-200"
-                    >
-                      <div className="text-base font-medium  gap-4  flex flex-grow justify-end items-center">
-                        <h1>{calendar.homeTeam?.name}</h1>
-                        {calendar?.homeTeam?.image ? (
-                          <img src={calendar.homeTeam.image} alt={'Team Logo'} />
-                        ) : (
-                          <TeamLogoDefault />
-                        )}
+                  .map((calendar, i) => (
+                    <div key={crypto.randomUUID()} className="flex flex-row gap-1 ">
+                      <div className="bg-radical-red p-2 my-2 w-12 grid place-content-center rounded-l-lg ">
+                        <h3 className="w-full h-full text-base sm:text-3xl  text-white text-center">{i + 1}</h3>
                       </div>
-                      <div className="flex bg-black/90 text-white gap-2 rounded items-center justify-center">
-                        <h1 className="text-lg font-bold">{calendar.homeScore}</h1>
-                        <h1 className="text-lg font-bold">-</h1>
-                        <h1 className="text-lg font-bold">{calendar.awayScore}</h1>
-                      </div>
-                      <div className="text-base flex flex-grow justify-center sm:justify-start gap-4 items-center ">
-                        {calendar?.homeTeam?.image ? (
-                          <img src={calendar?.homeTeam?.image} alt={'Team Logo'} />
-                        ) : (
-                          <TeamLogoDefault />
-                        )}
-                        <h1 className="font-medium">{calendar.awayTeam?.name}</h1>
+                      <div className="rounded-r-xl w-full my-2 p-2 bg-radical-red text-white sm:py-2 flex flex-col sm:grid  items-center justify-center sm:grid-cols-3 sm:grid-rows-1 sm:gap-4">
+                        <div className=" w-full sm:gap-4 flex flex-grow justify-between sm:justify-end items-center">
+                          <h2 className="text-xs sm:text-lg text-center">{calendar.homeTeam?.name}</h2>
+                          {calendar?.homeTeam?.image ? (
+                            <img src={calendar.homeTeam.image} alt={'Team Logo'} />
+                          ) : (
+                            <TeamLogoDefault tailwind="fill-white w-[25px] sm:w-[50px]" />
+                          )}
+                        </div>
+                        <div className="sm:text-3xl bg-black text-radical-red w-full h-full flex gap-2 sm:rounded items-center justify-center">
+                          <h2 className="font-bold ">{calendar.homeScore}</h2>
+                          <h2 className="font-bold">-</h2>
+                          <h2 className="font-bold">{calendar.awayScore}</h2>
+                        </div>
+                        <div className="w-full flex sm:flex-row flex-row-reverse flex-grow justify-between sm:justify-start  items-center sm:gap-4 ">
+                          {calendar?.homeTeam?.image ? (
+                            <img src={calendar?.homeTeam?.image} alt={'Team Logo'} />
+                          ) : (
+                            <TeamLogoDefault tailwind="fill-white w-[25px] sm:w-[50px]" />
+                          )}
+                          <h2 className="text-xs sm:text-lg text-center">{calendar.awayTeam?.name}</h2>
+                        </div>
                       </div>
                     </div>
                   ))}
               </div>
-              <div className="flex flex-col items-center mt-4 justify-center bg-blue-500">
-                <h1 className="text-center text-xl font-bold ">Descansan</h1>
+              <div className="flex flex-col items-center mt-4 justify-center bg-transparent">
+                <h1 className="text-start text-2xl font-bold w-full">Descansan</h1>
+
                 {dataCalendars?.calendars
                   ?.filter(t => t.awayTeamId === null)
-                  .map(calendar => (
-                    <div
-                      key={crypto.randomUUID()}
-                      className="my-1 w-full flex flex-row flex-wrap justify-center items-center gap-4 py-2 bg-rose-500 hover:bg-gray-200"
-                    >
-                      <h1>{calendar.homeTeam?.name}</h1>
-                      {calendar?.homeTeam?.image ? (
-                        <img src={calendar?.homeTeam?.image} alt={'Team Logo'} />
-                      ) : (
-                        <TeamLogoDefault />
-                      )}
+                  .map((calendar, i) => (
+                    <div key={crypto.randomUUID()} className="flex flex-row gap-1 w-full">
+                      <div className="rounded-l-lg bg-radical-red p-2 my-1 w-12 grid place-content-center">
+                        <h3 className="w-full h-full text-base sm:text-3xl  text-white text-center">{i + 1}</h3>
+                      </div>
+                      <div className="rounded-r-lg my-1 w-full flex flex-row flex-wrap justify-start items-center gap-4 p-2 bg-radical-red text-white">
+                        {calendar?.homeTeam?.image ? (
+                          <img src={calendar?.homeTeam?.image} alt={'Team Logo'} />
+                        ) : (
+                          <TeamLogoDefault tailwind="fill-white w-[33px] sm:w-[50px]" />
+                        )}
+                        <h2 className=" text-xs sm:text-xl">{calendar.homeTeam?.name}</h2>
+                      </div>
                     </div>
                   ))}
               </div>
