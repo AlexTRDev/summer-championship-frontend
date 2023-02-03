2161ef1488c514e3e3f98ce7d0c1be7832bcb09f
Alex Terrones Rivera <allex_tr@hotmail.com>
2023-02-02 20:47:50 -0500

    Update StatsLayout.tsx

--- src/layouts/StatsLayout.tsx
+++ src/layouts/StatsLayout.tsx
@@ -4,8 +4,8 @@ import { StatsNav } from '../components/Navbars/StatsNav'
 function StatsLayout() {
   return (
     <div>
-      <h1>Estadísticas</h1>
-      <div className="flex flex-row ">
+      <h1 className="sm:hidden">ESTADISTICAS</h1>
+      <div className="flex sm:flex-row flex-col">
         <StatsNav />
         <Outlet />
       </div>
