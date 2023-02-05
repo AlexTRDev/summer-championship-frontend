670d2875aaf1247d38bd8a51d64baa99d05cd577
Alex Terrones Rivera <allex_tr@hotmail.com>
2023-02-05 03:42:51 -0500

    Update LayoutApp.tsx

--- src/layouts/LayoutApp.tsx
+++ src/layouts/LayoutApp.tsx
@@ -1,15 +1,17 @@
+import { useState } from 'react'
 import { Outlet } from 'react-router'
 import { Navbar } from '../components'
 import { Logos } from '../components/Logos'
 
 function LayaoutApp() {
+  const [toggleMenu, setToggleMenu] = useState(false)
   return (
     <>
       <div>
         <Logos />
-        <Navbar />
+        <Navbar toggle={toggleMenu} setToggle={setToggleMenu} />
       </div>
-      <div>
+      <div className={`${toggleMenu ? 'hidden' : ''}`}>
         <Outlet />
       </div>
     </>
