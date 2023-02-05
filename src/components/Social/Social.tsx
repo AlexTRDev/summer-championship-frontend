41e1b0e9e2ec1e6d2790dcda073bd2f909f7ea67
Alex Terrones Rivera <allex_tr@hotmail.com>
2023-02-05 03:42:22 -0500

    Update Social.tsx

--- src/components/Social/Social.tsx
+++ src/components/Social/Social.tsx
@@ -7,10 +7,10 @@ export interface SocialInterface {}
 const Social: React.FC<SocialInterface> = () => {
   return (
     <div className="flex flex-row items-center justify-evenly gap-4 p-2">
-      <BsInstagram className="h-[20px] w-[20px] hover:cursor-pointer fill-black hover:fill-white" />
-      <AiFillGithub className="h-[20px] w-[20px] hover:cursor-pointer fill-black hover:fill-white" />
-      <BsFacebook className="h-[20px] w-[20px] hover:cursor-pointer fill-black hover:fill-white" />
-      <BsWhatsapp className="h-[20px] w-[20px] hover:cursor-pointer fill-black hover:fill-white" />
+      <BsInstagram className="h-[20px] w-[20px] hover:cursor-pointer fill-white hover:fill-black " />
+      <AiFillGithub className="h-[20px] w-[20px] hover:cursor-pointer fill-white hover:fill-black " />
+      <BsFacebook className="h-[20px] w-[20px] hover:cursor-pointer fill-white hover:fill-black " />
+      <BsWhatsapp className="h-[20px] w-[20px] hover:cursor-pointer fill-white hover:fill-black " />
     </div>
   )
 }
