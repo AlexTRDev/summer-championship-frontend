b86179638ace140cedaa7f09b02431c10582a83a
Alex Terrones Rivera <allex_tr@hotmail.com>
2023-02-05 03:43:06 -0500

    Update RoutesApp.routes.tsx

--- src/routes/RoutesApp.routes.tsx
+++ src/routes/RoutesApp.routes.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom'
-import { Asistaints, Classification, MVP, Scorers } from '../components'
-import { CalendarLayout, EquiposLayout, LayoutApp, PlayersLayout, StatsLayout } from '../layouts'
+import { Asistaints, Classification, MVP, Player, Scorers } from '../components'
+import { CalendarLayout, EquiposLayout, LayoutApp, PlayersLayout, ReglamentoLayout, StatsLayout } from '../layouts'
+import Contacto from '../layouts/Contacto'
 
 import ProtectedRoutes from './ProtectedRoutes.routes'
 
@@ -17,9 +18,13 @@ export const router = createBrowserRouter(
         <Route path="goleadores" element={<Scorers />} />
         <Route path="asistencias" element={<Asistaints />} />
       </Route>
-      <Route path="Contacto" element={<h1>Contacto</h1>} />
-      <Route path="Reglamento" element={<h1>Reglamento</h1>} />
-      <Route path="LaPolla" element={<h1>LaPolla</h1>} />
+      <Route path="Contacto" element={<Contacto />} />
+      <Route path="Sobre-mi" element={<Player />} />
+      <Route path="Reglamento" element={<ReglamentoLayout />} />
+      <Route
+        path="LaPolla"
+        element={<h1 className="grid h-96 place-content-center text-gray-500 text-xl">... Muy pronto ...</h1>}
+      />
       <Route path="register" element={<h1>register</h1>} />
       <Route element={<ProtectedRoutes />}>
         <Route path="profile" element={<h1>profile</h1>} />
