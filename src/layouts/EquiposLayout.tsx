cff85246918c7c7f926f43677d0a302f2be71def
Alex Terrones Rivera <allex_tr@hotmail.com>
2023-02-05 03:42:45 -0500

    Update EquiposLayout.tsx

--- src/layouts/EquiposLayout.tsx
+++ src/layouts/EquiposLayout.tsx
@@ -7,10 +7,12 @@ function EquiposLayout() {
 
   if (isLoading) return <h1>Loading...</h1>
   if (error) return <h1>Error en la petición</h1>
+
   return (
     <>
       <Outlet />
-      <div className="flex flex-row flex-wrap justify-center items-center content-center gap-8 py-10">
+      <h1 className="text-2xl font-medium pt-2 pl-2 sm:hidden text-gray-500">Equipos</h1>
+      <div className="flex flex-row flex-wrap justify-center items-center content-center gap-8 py-10 w-full px-4">
         {data?.teams.map((team, i) => (
           <TeamCard key={crypto.randomUUID()} team={team} linear={i % 6} />
         ))}
