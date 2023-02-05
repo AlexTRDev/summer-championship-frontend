fe6d50fab66ff16d4da3b800186ae18e63f2bb22
Alex Terrones Rivera <allex_tr@hotmail.com>
2023-02-05 03:41:50 -0500

    Update TeamCard.tsx

--- src/components/Cards/TeamCard.tsx
+++ src/components/Cards/TeamCard.tsx
@@ -13,22 +13,28 @@ const TeamCard: React.FC<TeamCardInterface> = ({ team, linear }) => {
   const color = colors[linear]
   const shadow = shadows[linear]
   const fill = fills[linear]
+
+  // no olvidar  nunca xd
+  const handleClick = () => {
+    if (window.screen.width > 900) window.scrollTo({ top: 121, behavior: 'smooth' })
+    else window.scrollTo({ top: 64, behavior: 'smooth' })
+  }
+
   return (
-    <>
-      <NavLink to={`${team.id}`}>
-        <div
-          className={`grid grid-cols-3 gap-4 w-96 h-32 items-center rounded-lg shadow-md ${shadow} ${color} ${gradient} relative overflow-hidden transition-all delay-150 hover:-translate-y-1 hover:scale-110 hover:cursor-pointer duration-200`}
-        >
-          {team.image ? (
-            <img src={team.image} alt={'Team Logo'} />
-          ) : (
-            <TeamLogoDefault tailwind="fill-white w-[100px] h-[100px] pl-2 z-10" />
-          )}
-          <TeamLogoDefault tailwind={`${fill} absolute w-[380px] h-[380px] -top-32 left-0`} />
-          <h3 className="text-white font-black col-span-2 z-10">{team.name}</h3>
-        </div>
-      </NavLink>
-    </>
+    <NavLink to={`${team.id}`}>
+      <div
+        className={`grid grid-cols-3 p-2 gap-4 w-80 sm:w-96 h-32 items-center rounded-lg shadow-md ${shadow} ${color} ${gradient} relative overflow-hidden transition-all delay-150 sm:hover:-translate-y-1 sm:hover:scale-110 hover:cursor-pointer duration-200`}
+        onClick={handleClick}
+      >
+        {team.image ? (
+          <img src={team.image} alt={'Team Logo'} />
+        ) : (
+          <TeamLogoDefault tailwind="fill-white w-[100px] h-[100px] pl-2 z-10" />
+        )}
+        <TeamLogoDefault tailwind={`${fill} absolute w-[380px] h-[380px] -top-32 left-0`} />
+        <h3 className="text-white text-base font-medium sm:font-black col-span-2 z-10">{team.name}</h3>
+      </div>
+    </NavLink>
   )
 }
 
