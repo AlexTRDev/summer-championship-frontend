59844e26ac6e4e0d70168056faad6d43c74c8561
Alex Terrones Rivera <allex_tr@hotmail.com>
2023-02-05 23:54:49 -0500

    Update RoutesApp.routes.tsx

--- src/routes/RoutesApp.routes.tsx
+++ src/routes/RoutesApp.routes.tsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom'
-import { Asistaints, Classification, MVP, Player, Scorers } from '../components'
+import { Asistaints, Classification, Loader, MVP, Player, Scorers } from '../components'
 import { CalendarLayout, EquiposLayout, LayoutApp, PlayersLayout, ReglamentoLayout, StatsLayout } from '../layouts'
 import Contacto from '../layouts/Contacto'
 
@@ -23,9 +23,14 @@ export const router = createBrowserRouter(
       <Route path="Reglamento" element={<ReglamentoLayout />} />
       <Route
         path="LaPolla"
-        element={<h1 className="grid h-96 place-content-center text-gray-500 text-xl">... Muy pronto ...</h1>}
+        element={
+          <h1 className="grid place-content-center place-items-center h-56 text-gray-500 text-xl">
+            <span className="font-bold sm:hidden">LA POLLA</span>
+            ... Muy pronto ...
+          </h1>
+        }
       />
-      <Route path="register" element={<h1>register</h1>} />
+      <Route path="register" element={<Loader />} />
       <Route element={<ProtectedRoutes />}>
         <Route path="profile" element={<h1>profile</h1>} />
       </Route>
