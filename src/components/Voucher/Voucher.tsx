b0b11352e83b89615c8fa2ad83157fa5a08e2932
Alex Terrones Rivera <allex_tr@hotmail.com>
2023-02-10 10:01:49 -0500

    Update Voucher.tsx

--- src/components/Voucher/Voucher.tsx
+++ src/components/Voucher/Voucher.tsx
@@ -88,6 +88,7 @@ const Voucher: React.FC<IVoucher> = ({ data }) => {
         <button
           onClick={handleGeneratePDF}
           className="animate-bounce h-10 bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center"
+          disabled={Object.entries(data).length === 18}
         >
           <svg className="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
             <path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" />
