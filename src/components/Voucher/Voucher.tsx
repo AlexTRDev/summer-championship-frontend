f2b0d3cfd66bbb52a7a2a7b33d25aa76714c42bf
Alex Terrones Rivera <allex_tr@hotmail.com>
2023-02-11 05:57:39 -0500

    fixed disabled button and upper case name

--- src/components/Voucher/Voucher.tsx
+++ src/components/Voucher/Voucher.tsx
@@ -48,7 +48,7 @@ const Voucher: React.FC<IVoucher> = ({ data, jornada }) => {
 
     doc.setFontSize(10)
     doc.setTextColor('#aaa')
-    doc.text(`${name} - ${crypto.randomUUID().slice(0, 6)}`, 10, 20)
+    doc.text(`${name.toUpperCase()} - ${crypto.randomUUID().slice(0, 6)}`, 10, 20)
     doc.text(`Jornada Nº ${jornada}`, 100, 20)
 
     let y = 40
@@ -112,7 +112,7 @@ const Voucher: React.FC<IVoucher> = ({ data, jornada }) => {
         )}
         <button
           className="animate-bounce h-10 bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center"
-          // disabled={Object.entries(data).length !== 18}
+          disabled={Object.entries(data).length !== 18}
         >
           <svg className="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
             <path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" />
