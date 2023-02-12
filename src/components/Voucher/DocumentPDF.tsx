010343996e01d3bc0f344a4ab088bca93c001bb4
Alex Terrones Rivera <allex_tr@hotmail.com>
2023-02-12 13:59:12 -0500

    default select day 9

--- src/components/Voucher/DocumentPDF.tsx
+++ src/components/Voucher/DocumentPDF.tsx
@@ -12,7 +12,7 @@ const DocumentPDF = () => {
   const options = ['L', 'E', 'V']
 
   const [selectedOption, setSelectedOption] = useState(initialState)
-  const [selectDay, setSelectDay] = useState(8)
+  const [selectDay, setSelectDay] = useState(9)
 
   const { data, isLoading, error } = useGetCalendarsQuery({
     isInclude: 'isInclude=true',
