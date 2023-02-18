14c8cee9359ae77f5ece27b2b80b42dff51dcd05
Alex Terrones Rivera <allex_tr@hotmail.com>
2023-02-18 10:41:54 -0500

    Update user.d.ts

--- src/types/user.d.ts
+++ src/types/user.d.ts
@@ -1,9 +1,19 @@
+import { IResponse } from './response'
+
 export interface IUser {
   id?: number
   email?: string
   password?: string
   status: Status
   role: UserRole
-  name?: string
-  photo?: string
+  name: string
+  photo: string
+}
+
+export interface IResponseUser extends IResponse {
+  user: IUser
+}
+
+export interface IResponseUsers extends IResponse {
+  users: IUser[]
 }
