e25f2b3b942454f00ddb8e6748d0f5f8ae549990
Alex Terrones Rivera <allex_tr@hotmail.com>
2023-02-20 12:55:45 -0500

    Update prediction.d.ts

--- src/types/prediction.d.ts
+++ src/types/prediction.d.ts
@@ -1,5 +1,6 @@
 import { ICalendar } from './calendar'
 import { Result } from './enums'
+import { IResponse } from './response'
 
 export interface IPrediction {
   ticketId?: number
@@ -7,4 +8,23 @@ export interface IPrediction {
   result: Result | string
   createdAt?: string
   updatedAt?: string
+}
+
+export interface IPredictionRequest {
+  predictions: IPrediction[]
+  ticket: ITicket
+  token: string
+}
+
+export interface IRequestPrediction {
+  ticketId: number
+  calendarId: number
+  result?: string
+}
+
+export interface IResponsePrediction extends IResponse {
+  data: IPrediction
+}
+export interface IResponsePredictions extends IResponse {
+  data: IPrediction[]
 }
